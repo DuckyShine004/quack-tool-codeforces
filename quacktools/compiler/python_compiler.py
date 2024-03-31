1b0ac632ffaf5cc83eea3473cc47db050b38ee17
Gallon Zhou <gallonzhou04@gmail.com>
2024-03-31 14:19:21 +1300

    feat: Added Support For Python Code

--- quacktools/compiler/python_compiler.py
+++ quacktools/compiler/python_compiler.py
@@ -1,4 +1,4 @@
-import subprocess
+import os
 
 from quacktools.compiler.compiler import Compiler
 
@@ -10,7 +10,7 @@ class PythonCompiler(Compiler):
     def get_program_output(self) -> None:
         """Get the user's program's code output."""
 
-        command = f"python3 -m {self.file}"
+        command = ["python3", self.file]
 
         for sample_input in self.samples["input"]:
             sample_input = "".join(sample_input).strip()
